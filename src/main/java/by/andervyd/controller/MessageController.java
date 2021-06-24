07a59b99b201b1b5f50fd292cc964fe4a7ea08a6
andervyd <andervyd@gmail.com>
2021-06-24 06:31:01 +0300

    add controller: MessageController

---
+++ src/main/java/by/andervyd/controller/MessageController.java
@@ -0,0 +1,27 @@
+package by.andervyd.controller;
+
+import by.andervyd.model.MessageModel;
+import by.andervyd.storage.UserStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat/{to}")
+    public void setMessage(@DestinationVariable String to, MessageModel message) {
+        System.out.println("handling send message: " + message + " to: " + to);
+
+        boolean isExists = UserStorage.getInstance()
+                                      .getUsers()
+                                      .contains(to);
+        if (isExists) {
+            simpMessagingTemplate.convertAndSend("/topic/messages/" + to, message);
+        }
+    }
+}
